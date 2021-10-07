cecade6673bcb100d9c202e35c3759d29f9ee266
Hansss <79178335+5Hyeons@users.noreply.github.com>
2021-10-07 12:46:17 +0900

    test: d2 지우고 d로 통합

--- 모음 사전/hans.py
+++ 모음 사전/hans.py
@@ -1,15 +1,14 @@
 def solution(word):
     answer = 0
     st = ''
-    d1 = {'A':1, 'E':2, 'I':3, 'O':4, 'U':5}
-    d2 = {1:'A', 2:'E', 3:'I', 4:'O', 5:'U'}
+    d = {'A':'E', 'E':'I', 'I':'O', 'O':'U'}
     while st!=word:
         if len(st)<5:
             st+='A'
         else:
             while st[-1] == 'U':
                 st = st[:-1]
-            st = st[:-1] + d2[d1[st[-1]]+1]
+            st = st[:-1] + d[st[-1]]
         # print(st)
         answer += 1
     return answer
