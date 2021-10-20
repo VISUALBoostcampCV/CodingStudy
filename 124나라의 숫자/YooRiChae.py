ee1257a4b6174f9fbe1d3063844eed646a057543
YooRi Chae <78344298+yoorichae@users.noreply.github.com>
2021-10-20 12:49:06 +0900

    Add: 124나라의 숫자/YooRiChae.py

---
+++ 124나라의 숫자/YooRiChae.py
@@ -0,0 +1,47 @@
+def check(n):
+    if n%3==0: return 4
+    elif n%3==1: return 1
+    elif n%3==2: return 2
+    
+def test(num,new):
+    new.append(check(num))
+    if num <= 3:
+        return
+    if check(num) == 4:
+        num = int(num/3) - 1
+    else:
+        num=int(num/3)
+    test(num, new)
+    
+def solution(n):
+    answer = ''
+    new=[]
+    test(n, new)
+    new.reverse()
+    answer = "".join(list(map(str, new)))
+    return answer
+'''
+정확성  테스트
+테스트 1 〉	통과 (0.01ms, 10.2MB)
+테스트 2 〉	통과 (0.01ms, 10.2MB)
+테스트 3 〉	통과 (0.01ms, 10.2MB)
+테스트 4 〉	통과 (0.01ms, 10.3MB)
+테스트 5 〉	통과 (0.01ms, 10.2MB)
+테스트 6 〉	통과 (0.01ms, 10.3MB)
+테스트 7 〉	통과 (0.01ms, 10.3MB)
+테스트 8 〉	통과 (0.01ms, 10.2MB)
+테스트 9 〉	통과 (0.01ms, 10.2MB)
+테스트 10 〉	통과 (0.01ms, 10.3MB)
+테스트 11 〉	통과 (0.01ms, 10.1MB)
+테스트 12 〉	통과 (0.01ms, 10.3MB)
+테스트 13 〉	통과 (0.01ms, 10.2MB)
+테스트 14 〉	통과 (0.01ms, 10.2MB)
+
+효율성  테스트
+테스트 1 〉	통과 (0.02ms, 10.1MB)
+테스트 2 〉	통과 (0.02ms, 10.1MB)
+테스트 3 〉	통과 (0.02ms, 10.3MB)
+테스트 4 〉	통과 (0.03ms, 10.1MB)
+테스트 5 〉	통과 (0.02ms, 10.3MB)
+테스트 6 〉	통과 (0.03ms, 10.1MB)
+'''
