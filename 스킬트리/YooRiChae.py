9a895b3781d14ce5d14ccdb86e4a936b085613a2
YooRi Chae <78344298+yoorichae@users.noreply.github.com>
2021-10-28 11:44:16 +0900

    feat: Create 스킬트리 folder, Create YooRiChae.py

---
+++ 스킬트리/YooRiChae.py
@@ -0,0 +1,32 @@
+def solution(skill, skill_trees):
+    answer = 0
+    for user_skill in skill_trees:
+        check = skill
+        for s in user_skill:
+            if s not in check:
+                continue
+            elif s == check[0]:
+                check=check[1:]
+            else:
+                answer-=1
+                break
+        answer+=1
+    return answer
+
+'''
+정확성  테스트
+테스트 1 〉	통과 (0.00ms, 10.2MB)
+테스트 2 〉	통과 (0.00ms, 10.2MB)
+테스트 3 〉	통과 (0.01ms, 10.2MB)
+테스트 4 〉	통과 (0.01ms, 10.2MB)
+테스트 5 〉	통과 (0.01ms, 10.2MB)
+테스트 6 〉	통과 (0.01ms, 10.2MB)
+테스트 7 〉	통과 (0.01ms, 10.2MB)
+테스트 8 〉	통과 (0.01ms, 10.2MB)
+테스트 9 〉	통과 (0.01ms, 10.2MB)
+테스트 10 〉	통과 (0.01ms, 10.2MB)
+테스트 11 〉	통과 (0.01ms, 10.2MB)
+테스트 12 〉	통과 (0.01ms, 10.2MB)
+테스트 13 〉	통과 (0.01ms, 10.2MB)
+테스트 14 〉	통과 (0.01ms, 10.2MB)
+'''
