c4827bc1671cf32d4418887c9778e00fcfe17bb3
Sangwoo Bae <42166742+wSangbae@users.noreply.github.com>
2021-11-04 17:34:50 +0900

    feat : modify bsw.py

--- 불량사용자/bsw.py
+++ 불량사용자/bsw.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 def match(user, ban):
     if len(user) != len(ban):
         return 0
@@ -9,57 +11,26 @@ def match(user, ban):
             return 0
     
     return 1
-    
 
+    
 def solution(user_id, banned_id):
     answer = 0
+
+    lst = [[] for _ in range(len(banned_id))]
     
-    answers = []
-    for user in user_id:
+    for _, user in enumerate(user_id):
         for i, ban in enumerate(banned_id):
-            banned_id_list = [0 for _ in range(len(banned_id))]
             if match(user, ban):
-                if answers:
-                    for ans in answers:
-                        if user not in ans and ans[i] == 0:
-                            ans[i] = user
-                
-                banned_id_list[i] = user
-                answers.append(banned_id_list)
+                lst[i].append(user)
+
     
-    print(answers)
-    s=set()
-    for ans in answers:
-        if 0 not in ans:
-            s.add(tuple(ans))
-        
-              
-        # frodo
-        #         frodo
-        # fradi
-        #         crodo
-        #                 abc123
-        #                         abc123
-        #                 frodoc
-        #                         frodoc
-        
-                
+    ids = list(product(*lst))
+    answers = set()
     
-    return len(s)
-  
-  
-  '''
-  fail
-     
-테스트 1 〉	통과 (0.03ms, 10.3MB)
-테스트 2 〉	실패 (0.09ms, 10.3MB)
-테스트 3 〉	실패 (0.08ms, 10.3MB)
-테스트 4 〉	실패 (0.05ms, 10.2MB)
-테스트 5 〉	실패 (0.62ms, 10.2MB)
-테스트 6 〉	실패 (0.12ms, 10.2MB)
-테스트 7 〉	실패 (0.08ms, 10.3MB)
-테스트 8 〉	실패 (0.06ms, 10.2MB)
-테스트 9 〉	실패 (0.08ms, 10.3MB)
-테스트 10 〉	통과 (0.12ms, 10.3MB)
-테스트 11 〉	실패 (0.08ms, 10.3MB)
-'''
+    for id_ in ids:
+        if len(id_) == len(set(id_)):
+            answers.add(tuple(set(sorted(id_))))
+
+    # print(answers)
+    
+    return len(answers)
