a4f24412f317144fa3b00e5dede3faac2a727a18
Hansss <79178335+5Hyeons@users.noreply.github.com>
2021-11-09 11:22:23 +0900

    add: hans.py [failed]

---
+++ 금과 은 운반하기/hans.py
@@ -0,0 +1,55 @@
+import numpy as np
+from collections import defaultdict
+
+def solution(a, b, g, s, w, t):
+    answer = 0
+    d = defaultdict(int)
+    t_np = np.array(t)
+    last = 0
+    while a>0 or b>0:
+        t_min = np.min(t_np)
+        for i, _t in enumerate(t_np):
+            new_t = _t-t_min
+            if new_t <= 0:
+                _g = min(a,g[i],w[i])
+                _s = min(b,s[i],w[i]-_g)
+                a -= _g
+                b -= _s
+                new_t = t[i]
+                d[i] += 1
+            t_np[i] = new_t
+            last = i
+    
+    print(d)
+    
+    return max([2*t[k]*v for k,v in d.items()]) - t[last]
+# a,b,g,s,w,t = 10, 10, [100], [100], [7], [10]
+a,b,g,s,w,t = 90,500,[70,70,0],[0,0,500],[100,100,2],[4,8,1]
+s = solution(a,b,g,s,w,t)
+print(s)
+'''
+테스트 1 〉	통과 (8.01ms, 27.5MB)
+테스트 2 〉	실패 (시간 초과)
+테스트 3 〉	통과 (6643.81ms, 27.9MB)
+테스트 4 〉	실패 (9193.64ms, 27.8MB)
+테스트 5 〉	실패 (9248.71ms, 28MB)
+테스트 6 〉	실패 (시간 초과)
+테스트 7 〉	실패 (시간 초과)
+테스트 8 〉	실패 (시간 초과)
+테스트 9 〉	실패 (시간 초과)
+테스트 10 〉	실패 (시간 초과)
+테스트 11 〉	실패 (시간 초과)
+테스트 12 〉	실패 (시간 초과)
+테스트 13 〉	실패 (시간 초과)
+테스트 14 〉	실패 (시간 초과)
+테스트 15 〉	실패 (시간 초과)
+테스트 16 〉	실패 (시간 초과)
+테스트 17 〉	실패 (시간 초과)
+테스트 18 〉	실패 (시간 초과)
+테스트 19 〉	실패 (시간 초과)
+테스트 20 〉	실패 (시간 초과)
+테스트 21 〉	실패 (시간 초과)
+테스트 22 〉	실패 (시간 초과)
+테스트 23 〉	실패 (시간 초과)
+테스트 24 〉	통과 (0.36ms, 27.8MB)
+'''
