dd67041d14f8002b5731e8ae9454f1ae6ae978e4
Juyoung <jk042386@gmail.com>
2021-12-08 15:45:03 +0900

    add: 순위 검색/jujoo.py 못해먹겠네

---
+++ [211207] 순위 검색/jujoo.py
@@ -0,0 +1,65 @@
+# 효율성 탈락
+import pandas as pd
+def solution(info, query):
+    answer = []
+    infos = [i.split() for i in info]
+    df = pd.DataFrame(infos, columns=['l', 'j', 'y', 'f', 's'])
+    df['s'] = df['s'].astype('int')
+
+    for q in query:
+        q, score = q.rsplit(' ', 1)
+        score = int(score)
+        l, j, y, f = q.split(' and ')
+        sub_df = df
+        for key, val in [('l', l), ('j', j), ('y', y), ('f', f), ('s', int(score))]:
+            if isinstance(val, int):
+                answer.append(sum(sub_df[key] >= val))
+            if val != '-':
+                sub_df = sub_df[sub_df[key] == val]
+    return answer
+
+# 효율성 탈락
+import re
+def solution(info, query):
+    answer = []
+    infos = []
+    for i in info:
+        q, score = i.rsplit(' ', 1)
+        q = ' and '.join(q.split())
+        infos.append((q, int(score)))
+
+    for q in query:
+        q, score = q.rsplit(' ', 1)
+        q = q.replace('-', '.+')
+        answer.append(len(list(filter(lambda x: len(re.findall(q, x[0])) and x[1] >= int(score), infos))))
+    return answer
+
+# 효율성 탈락
+from collections import defaultdict
+def solution(info, query):
+    answer = []
+    data = defaultdict(list)
+    for l in ['cpp', 'java', 'python', '-']:
+        for j in ['backend', 'frontend', '-']:
+            for y in ['junior', 'senior', '-']:
+                for f in ['chicken', 'pizza', '-']:
+                    data[(l, j, y, f)] = []
+    
+    for i in info:
+        q, s = i.rsplit(' ', 1)
+        l, j, y, f = q.split()
+        for a in [l, '-']:
+            for b in [j, '-']:
+                for c in [y, '-']:
+                    for d in [f, '-']:
+                        if (a, b, c, d) in data:
+                            data[(a, b, c, d)].append(((a, b, c, d), int(s)))
+    
+    for q in query:
+        q, s = q.rsplit(' ', 1)
+        cnt = 0
+        for _, score in data[tuple(q.split(' and '))]:
+            if score >= int(s):
+                cnt += 1
+        answer.append(cnt)
+    return answer
