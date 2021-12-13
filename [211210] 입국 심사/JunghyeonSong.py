f849c4494c4feeb27cb10136271dc2a3bf1c8513
Junghyeon Song <gldkzm@gmail.com>
2021-12-13 13:39:10 +0900

    chore: 입국 심사/JunghyeonSong.py

--- [211210] 입국 심사/JunghyeonSong.py
+++ [211210] 입국 심사/JunghyeonSong.py
@@ -1,54 +1,38 @@
-import heapq as hq
-
 def solution(n, times):
     answer = 0
+    left = 1    # 최소 시간
+    right = n * max(times)  # 최대 시간
     
-    time_heap = list(zip([0]*len(times), times))
-    hq.heapify(time_heap)
+    # 이분탐색
+    while left < right:
+        mid = (left + right) // 2
+        total = 0
+        
+        # 각 심사위원이 주어진 시간동안 심사할 수 있는 인원 더하기
+        for time in times:
+            total += mid // time               
+        
+        # 심사할 수 있는 총 인원이 n 이상이면 왼쪽 반 탐색하기
+        if total >= n:
+            right = mid
+        # 심사할 수 있는 총 인원이 n보다 작으면 오른쪽 반 탐색하기
+        else:
+            left = mid + 1
     
-    while n:
-        wait, judge = hq.heappop(time_heap)
-        n_wait, n_judge = time_heap[0]
-        
-        # 처리시간 계산
-        judge_time = total_judge_time(wait, judge, answer)
-        n_judge_time = total_judge_time(n_wait, n_judge, answer)
-        
-        # 더 빠른 심사 있으면
-        if judge_time > n_judge_time:
-            n_wait, n_judge = hq.heappop(time_heap)
-            wait, n_wait = n_wait, wait
-            judge, n_judge = n_judge, judge
-            hq.heappush(time_heap, (n_wait, n_judge))
-        
-        n -= 1
-        hq.heappush(time_heap, (wait+judge, judge))        
-        answer = max(answer, wait+judge)    
-        
-        
+    answer = left   # 최소 시간
+    
     return answer
-
-
-# 심사 대기시간 + 처리시간 구하기
-def total_judge_time(wait, judge, answer):
-    total_time = wait + judge
-    
-    if answer % judge:
-        total_time += judge - answer % judge
-    
-    return total_time
-
 
 
 '''
 정확성  테스트
-테스트 1 〉	실패 (0.03ms, 10.3MB)
-테스트 2 〉	실패 (1.53ms, 10.2MB)
-테스트 3 〉	실패 (10.23ms, 10.3MB)
-테스트 4 〉	실패 (239.12ms, 24.7MB)
-테스트 5 〉	실패 (891.40ms, 26MB)
-테스트 6 〉	실패 (시간 초과)
-테스트 7 〉	실패 (시간 초과)
-테스트 8 〉	실패 (시간 초과)
-테스트 9 〉	실패 (시간 초과)
+테스트 1 〉	통과 (0.01ms, 10.2MB)
+테스트 2 〉	통과 (0.10ms, 10.2MB)
+테스트 3 〉	통과 (3.71ms, 10.2MB)
+테스트 4 〉	통과 (276.01ms, 14.2MB)
+테스트 5 〉	통과 (414.98ms, 14.3MB)
+테스트 6 〉	통과 (332.12ms, 14.3MB)
+테스트 7 〉	통과 (546.60ms, 14.2MB)
+테스트 8 〉	통과 (517.43ms, 14.3MB)
+테스트 9 〉	통과 (0.04ms, 10.3MB)
 '''
